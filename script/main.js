75fa0ef904c9f5a6afd082703277b92dcb7ffcfc
Selly Yustina <127018283+Selly1534@users.noreply.github.com>
2024-06-09 10:10:40 +0700

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,18 @@
+// Clear form efore unload
+window.onbeforeunload = () =>{
+    for (const form of document.getElementsByTagName("form")){
+        form.reset();
+    }
+};
+
+const navSlide =() =>{
+    const burger = document.querySelector(".burger")
+    const navList = document.querySelector("nav")
+
+    burger.addEventListener("click", ()=>{
+        navList.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
